a0fa5ca7c3d487dbb751c6f06f36fbb3742329d3
DmitryS <sharygin_d@flowwow.com>
2026-09-07 00:26:29 +0300

    change NextConfig

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
+  // output: 'export', <-- Закомментируйте или удалите
   /* config options here */
 };
 
